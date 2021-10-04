4369e1a1d19fe93f098f6b91583c3f7d2009239c
Gilad Nadav <gilad.nadav@kaltura.com>
2021-10-04 15:57:17 +0300

    Initial module - android (#2)
    * add Calendar Module Example - Android

--- android/app/src/main/java/com/kalturaplayerrn/MainApplication.java
+++ android/app/src/main/java/com/kalturaplayerrn/MainApplication.java
@@ -20,14 +20,14 @@ public class MainApplication extends Application implements ReactApplication {
           return BuildConfig.DEBUG;
         }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
-        }
+          @Override
+          protected List<ReactPackage> getPackages() {
+              @SuppressWarnings("UnnecessaryLocalVariable")
+              List<ReactPackage> packages = new PackageList(this).getPackages();
+              // below MyAppPackage is added to the list of packages returned
+              packages.add(new CalendarPackage());
+              return packages;
+          }
 
         @Override
         protected String getJSMainModuleName() {
