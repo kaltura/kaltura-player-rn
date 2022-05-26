3645af386a71c6620b3db9f94ec5be9e629670be
Gourav Saxena <gourav.saxena@kaltura.com>
2022-05-26 14:42:50 +0530

    - Converted to const fields

--- android/src/main/java/com/reactnativekalturaplayer/events/KalturaPlayerAdEvents.kt
+++ android/src/main/java/com/reactnativekalturaplayer/events/KalturaPlayerAdEvents.kt
@@ -3,77 +3,41 @@ package com.reactnativekalturaplayer.events
 class KalturaPlayerAdEvents {
 
     companion object {
-        @JvmField
-        val AD_REQUESTED = "adRequested"
-        @JvmField
-        val AD_FIRST_PLAY = "adFirstPlay"
-        @JvmField
-        val STARTED = "started"
-        @JvmField
-        val AD_DISPLAYED_AFTER_CONTENT_PAUSE = "adDisplayedAfterContentPause" // Not in USE in SDK
-        @JvmField
-        val PAUSED = "paused"
-        @JvmField
-        val RESUMED = "resumed"
-        @JvmField
-        val COMPLETED = "completed"
-        @JvmField
-        val FIRST_QUARTILE = "firstQuartile"
-        @JvmField
-        val MIDPOINT = "midpoint"
-        @JvmField
-        val THIRD_QUARTILE = "thirdQuartile"
-        @JvmField
-        val SKIPPED = "skipped"
-        @JvmField
-        val SKIPPABLE_STATE_CHANGED = "skippableStateChanged"
-        @JvmField
-        val CLICKED = "adClickedEvent"
-        @JvmField
-        val TAPPED = "tapped"
-        @JvmField
-        val ICON_FALLBACK_IMAGE_CLOSED = "iconFallbackImageClosed"
-        @JvmField
-        val ICON_TAPPED = "iconTapped"
-        @JvmField
-        val AD_BREAK_READY = "adBreakReady"
-        @JvmField
-        val AD_PROGRESS = "adProgress"
-        @JvmField
-        val AD_BREAK_STARTED = "adBreakStarted"
-        @JvmField
-        val AD_BREAK_ENDED = "adBreakEnded"
-        @JvmField
-        val AD_BREAK_FETCH_ERROR = "adBreakFetchError"
-        @JvmField
-        val AD_BREAK_IGNORED = "adBreakIgnored"
-        @JvmField
-        val CUEPOINTS_CHANGED = "cuepointsChanged"
-        @JvmField
-        val PLAY_HEAD_CHANGED = "playHeadChanged"
-        @JvmField
-        val LOADED = "loaded"
-        @JvmField
-        val CONTENT_PAUSE_REQUESTED = "contentPauseRequested"
-        @JvmField
-        val CONTENT_RESUME_REQUESTED = "contentResumeRequested"
-        @JvmField
-        val ALL_ADS_COMPLETED = "allAdsCompleted"
-        @JvmField
-        val AD_LOAD_TIMEOUT_TIMER_STARTED = "adLoadTimeoutTimerStarted" // Not in USE in SDK
-        @JvmField
-        val AD_BUFFER_START = "adBufferStart"
-        @JvmField
-        val AD_BUFFER_END = "adBufferEnd"
-        @JvmField
-        val AD_PLAYBACK_INFO_UPDATED = "adPlaybackInfoUpdated"
-        @JvmField
-        val ERROR = "error"
-        @JvmField
-        val DAI_SOURCE_SELECTED = "daiSourceSelected"
-        @JvmField
-        val AD_WATERFALLING = "adWaterFalling" // Not required until we has AdLayout feature in RN
-        @JvmField
-        val AD_WATERFALLING_FAILED = "adWaterFallingFailed" // Not required until we has AdLayout feature in RN
+        const val AD_REQUESTED = "adRequested"
+        const val AD_FIRST_PLAY = "adFirstPlay"
+        const val STARTED = "started"
+        const val AD_DISPLAYED_AFTER_CONTENT_PAUSE = "adDisplayedAfterContentPause" // Not in USE in SDK
+        const val PAUSED = "paused"
+        const val RESUMED = "resumed"
+        const val COMPLETED = "completed"
+        const val FIRST_QUARTILE = "firstQuartile"
+        const val MIDPOINT = "midpoint"
+        const val THIRD_QUARTILE = "thirdQuartile"
+        const val SKIPPED = "skipped"
+        const val SKIPPABLE_STATE_CHANGED = "skippableStateChanged"
+        const val CLICKED = "adClickedEvent"
+        const val TAPPED = "tapped"
+        const val ICON_FALLBACK_IMAGE_CLOSED = "iconFallbackImageClosed"
+        const val ICON_TAPPED = "iconTapped"
+        const val AD_BREAK_READY = "adBreakReady"
+        const val AD_PROGRESS = "adProgress"
+        const val AD_BREAK_STARTED = "adBreakStarted"
+        const val AD_BREAK_ENDED = "adBreakEnded"
+        const val AD_BREAK_FETCH_ERROR = "adBreakFetchError"
+        const val AD_BREAK_IGNORED = "adBreakIgnored"
+        const val CUEPOINTS_CHANGED = "cuepointsChanged"
+        const val PLAY_HEAD_CHANGED = "playHeadChanged"
+        const val LOADED = "loaded"
+        const val CONTENT_PAUSE_REQUESTED = "contentPauseRequested"
+        const val CONTENT_RESUME_REQUESTED = "contentResumeRequested"
+        const val ALL_ADS_COMPLETED = "allAdsCompleted"
+        const val AD_LOAD_TIMEOUT_TIMER_STARTED = "adLoadTimeoutTimerStarted" // Not in USE in SDK
+        const val AD_BUFFER_START = "adBufferStart"
+        const val AD_BUFFER_END = "adBufferEnd"
+        const val AD_PLAYBACK_INFO_UPDATED = "adPlaybackInfoUpdated"
+        const val ERROR = "error"
+        const val DAI_SOURCE_SELECTED = "daiSourceSelected"
+        const val AD_WATERFALLING = "adWaterFalling" // Not required until we has AdLayout feature in RN
+        const val AD_WATERFALLING_FAILED = "adWaterFallingFailed" // Not required until we has AdLayout feature in RN
     }
 }
--- android/src/main/java/com/reactnativekalturaplayer/events/KalturaPlayerAnalyticsEvents.kt
+++ android/src/main/java/com/reactnativekalturaplayer/events/KalturaPlayerAnalyticsEvents.kt
@@ -3,12 +3,9 @@ package com.reactnativekalturaplayer.events
 class KalturaPlayerAnalyticsEvents {
 
     companion object {
-        @JvmField
-        val PHOENIX_CONCURRENCY_ERROR = "concurrencyError"
-        @JvmField
-        val PHOENIX_BOOKMARK_ERROR = "bookmarkError"
-        @JvmField
-        val PHOENIX_ERROR = "error"
+        const val PHOENIX_CONCURRENCY_ERROR = "concurrencyError"
+        const val PHOENIX_BOOKMARK_ERROR = "bookmarkError"
+        const val PHOENIX_ERROR = "error"
     }
 
 }
