f82691de3127921d8d64f66d635f935d80ebc5e6
Nilit Danan <nilit.danan@kaltura.com>
2022-06-17 11:18:10 +0100

    Missing files in commit

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -4,21 +4,22 @@ import { PlayerEvents } from './events/PlayerEvents';
 import { AdEvents } from './events/AdEvents';
 import { AnalyticsEvents } from './events/AnalyticsEvents';
 import { PLAYER_TYPE, MEDIA_FORMAT, MEDIA_ENTRY_TYPE, DRM_SCHEME, PLAYER_PLUGIN, PLAYER_RESIZE_MODES, WAKEMODE, SUBTITLE_STYLE, SUBTITLE_PREFERENCE, VIDEO_CODEC, AUDIO_CODEC, VR_INTERACTION_MODE } from './consts';
+export { PlayerEvents, AdEvents, AnalyticsEvents, PLAYER_TYPE, MEDIA_FORMAT, MEDIA_ENTRY_TYPE, DRM_SCHEME, PLAYER_PLUGIN, PLAYER_RESIZE_MODES, WAKEMODE, SUBTITLE_STYLE, SUBTITLE_PREFERENCE, VIDEO_CODEC, AUDIO_CODEC, VR_INTERACTION_MODE, };
 interface KalturaPlayerProps {
     style: ViewStyle;
     playerType: PLAYER_TYPE;
 }
-export { PlayerEvents, AdEvents, AnalyticsEvents, PLAYER_TYPE, MEDIA_FORMAT, MEDIA_ENTRY_TYPE, DRM_SCHEME, PLAYER_PLUGIN, PLAYER_RESIZE_MODES, WAKEMODE, SUBTITLE_STYLE, SUBTITLE_PREFERENCE, VIDEO_CODEC, AUDIO_CODEC, VR_INTERACTION_MODE, };
 export declare class KalturaPlayer extends React.Component<KalturaPlayerProps> {
     nativeComponentRef: any;
-    eventListener: any;
     playerType: PLAYER_TYPE | undefined;
     static propTypes: {
         style: object;
     };
     componentDidMount(): void;
     componentWillUnmount(): void;
-    setNativeProps: (nativeProps: any) => void;
+    render(): JSX.Element;
+}
+export declare class KalturaPlayerAPI {
     /**
      * This method creates a Player instance internally (Basic, OVP/OTT Player)
      * With this, it take the PlayerInitOptions which are having essential Player settings values
@@ -28,7 +29,7 @@ export declare class KalturaPlayer extends React.Component<KalturaPlayerProps> {
      * should be always greater than 0 and should be valid otherwise, we will not be able to featch the details
      * for the mediaId or the entryId)
      */
-    setup: (options: string, isPlayerCreated: Function, id?: number) => void;
+    static setup: (id: number | undefined, options: string) => Promise<any>;
     /**
      * Load the media with the given
      *
@@ -41,66 +42,66 @@ export declare class KalturaPlayer extends React.Component<KalturaPlayerProps> {
      * EntryId for Kaltura OVP Player
      * @param asset Media Asset JSON String
      */
-    loadMedia: (id: string, asset: string) => void;
+    static loadMedia: (id: string, asset: string) => void;
     /**
      * Add the listners for the Kaltura Player
      */
-    addListeners: () => void;
+    static addListeners: () => void;
     /**
      * Add the listners for the Kaltura Player
      */
-    removeListeners: () => void;
+    static removeListeners: () => void;
     /**
      * Should be called when the application is in background
      */
-    onApplicationPaused: () => void;
+    static onApplicationPaused: () => void;
     /**
      * Should be called when the application comes back to
      * foreground
      */
-    onApplicationResumed: () => void;
+    static onApplicationResumed: () => void;
     /**
      * Update a Plugin Config
      *
      * @param pluginName Plugin Name (Youbora, IMA etc)
      * @param config Updated Plugin Config (YouboraConfig JSON, IMAConfig JSON etc)
      */
-    updatePluginConfig: (pluginName: PLAYER_PLUGIN, config: object) => void;
+    static updatePluginConfig: (pluginName: PLAYER_PLUGIN, config: object) => void;
     /**
      * Play the player if it is not playing
      */
-    play: () => void;
+    static play: () => void;
     /**
      * Pause the player if it is playing
      */
-    pause: () => void;
+    static pause: () => void;
     /**
      * Stops the player to the initial state
      */
-    stop: () => void;
+    static stop: () => void;
     /**
      * Destroy the Kaltura Player instance
      */
-    destroy: () => void;
+    static destroy: () => void;
     /**
      * Replays the media from the beginning
      */
-    replay: () => void;
+    static replay: () => void;
     /**
      * Seek the player to the specified position
      * @param position in miliseconds (Ms)
      */
-    seekTo: (position: number) => void;
+    static seekTo: (position: number) => void;
     /**
      * Change a specific track (Video, Audio or Text track)
      * @param trackId Unique track ID which was sent in `tracksAvailable` event
      */
-    changeTrack: (trackId: string) => void;
+    static changeTrack: (trackId: string) => void;
     /**
      * Change the playback rate (ff or slow motion). Default is 1.0f
      * @param rate Desired playback rate (Ex: 0.5f, 1.5f 2.0f etc)
      */
-    setPlaybackRate: (rate: number) => void;
+    static setPlaybackRate: (rate: number) => void;
     /**
      * Change the volume of the current audio track.
      * Accept values between 0.0 and 1.0. Where 0.0 is mute and 1.0 is maximum volume.
@@ -109,20 +110,20 @@ export declare class KalturaPlayer extends React.Component<KalturaPlayerProps> {
      *
      * @param vol - volume to set.
      */
-    setVolume: (vol: number) => void;
+    static setVolume: (vol: number) => void;
     /**
      * Set the media to play automatically at the start (load)
      * if false, user will have to click on UI play button
      *
      * @param isAutoPlay media should be autoplayed at the start or not
      */
-    setAutoPlay: (isAutoPlay: boolean) => void;
+    static setAutoPlay: (isAutoPlay: boolean) => void;
     /**
      * Set the KS for the media (only for OVP/OTT users)
      * Call this before calling {@link loadMedia}
      * @param KS Kaltura Secret key
      */
-    setKS: (KS: string) => void;
+    static setKS: (KS: string) => void;
     /**
      * NOOP
      * @param index
@@ -131,32 +132,46 @@ export declare class KalturaPlayer extends React.Component<KalturaPlayerProps> {
      * Only for Live Media.
      * Seek player to Live Default Position.
      */
-    seekToLiveDefaultPosition: () => void;
+    static seekToLiveDefaultPosition: () => void;
     /**
      * Update the existing subtitle styling
      */
-    updateSubtitleStyle: (subtitleStyle: string) => void;
+    static updateSubtitleStyle: (subtitleStyle: string) => void;
     /**
      * Update the Resize Mode
      */
-    updateResizeMode: (mode: PLAYER_RESIZE_MODES) => void;
+    static updateResizeMode: (mode: PLAYER_RESIZE_MODES) => void;
     /**
      * Update the ABR Settings
      */
-    updateAbrSettings: (abrSettings: string) => void;
+    static updateAbrSettings: (abrSettings: string) => void;
     /**
      * Reset the ABR Settings
      */
-    resetAbrSettings: () => void;
+    static resetAbrSettings: () => void;
     /**
      * Update the Low Latency Config
      * Only for Live Media
      */
-    updateLowLatencyConfig: (lowLatencyConfig: string) => void;
+    static updateLowLatencyConfig: (lowLatencyConfig: string) => void;
     /**
      * Reset the Low Latency Config
      * Only for Live Media
      */
-    resetLowLatencyConfig: () => void;
-    render(): JSX.Element;
+    static resetLowLatencyConfig: () => void;
+    /**
+     * Getter for the current playback position.
+     * @returns string: Position of the player or {@link POSITION_UNSET}
+     */
+    static getCurrentPosition: () => Promise<any>;
+    /**
+     * Checks if Player is currently playing or not
+     * @returns boolean
+     */
+    static isPlaying: () => Promise<any>;
+    /**
+     * Checks if the stream is Live or Not
+     * @returns boolean
+     */
+    static isLive: () => Promise<any>;
 }
