f99fafd32e5ff922f1522fab4022a53b106845ea
Nilit Danan <nilit.danan@kaltura.com>
2022-09-13 14:43:56 +0100

    Updated lib folder with last merge after yarn install.

--- lib/typescript/consts.d.ts
+++ lib/typescript/consts.d.ts
@@ -96,3 +96,8 @@ export declare enum VR_INTERACTION_MODE {
     CARD_BOARD_MOTION = "CardboardMotion",
     CARD_BOARD_MOTION_WITH_TOUCH = "CardboardMotionWithTouch"
 }
+export declare enum TRACK_SELECTION_MODE {
+    OFF = "OFF",
+    AUTO = "AUTO",
+    SELECTION = "SELECTION"
+}
