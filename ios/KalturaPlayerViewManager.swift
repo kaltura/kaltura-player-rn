92546b498c8a3fc478fb882994a53f332fcb8284
nikita pavlovich <nikita.pavlovich@kaltura.com>
2023-06-27 17:17:27 +0300

    KUXRN-4356: update defaults

--- ios/KalturaPlayerViewManager.swift
+++ ios/KalturaPlayerViewManager.swift
@@ -556,13 +556,13 @@ class KalturaPlayerRNView : UIView {
     func updateOttAnalyticsConfig(config: Dictionary<String, Any>) {
         let baseUrl: String = config["baseUrl"] as? String ?? ""
         
-        let timerInterval: Double = config["timerInterval"] as? Double ?? 0.0
+        let timerInterval: Double = config["timerInterval"] as? Double ?? 30.0
         
         let ks: String = config["ks"] as? String ?? ""
         
         let partnerId: Int = config["partnerId"] as? Int ?? 0
         
-        let isExperimentalLiveMediaHit: Bool = config["experimentalLiveMediaHit"] as? Bool ?? false
+        let isExperimentalLiveMediaHit: Bool = config["experimentalLiveMediaHit"] as? Bool ?? true
         
         let ottAnalyticsConfig = PhoenixAnalyticsPluginConfig(baseUrl: baseUrl, timerInterval: timerInterval, ks: ks, partnerId: partnerId, isExperimentalLiveMediaHit: isExperimentalLiveMediaHit)
         
